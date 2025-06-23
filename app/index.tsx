87b406ecf36c067731c5c466dfacfc40ba29ae7c
Muhammad Usama <mohammad.usama441@gmail.com>
2025-06-23 14:46:18 +0500

    First Push

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import MainScreen from '../root/Screens/MainScreen'
+
+const RootPage: React.FC = () => {
+  return <MainScreen/>
+}
+
+export default RootPage
---
+++ root/Screens/MainScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>MainScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default MainScreen
