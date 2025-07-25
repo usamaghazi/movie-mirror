6ddf4f8800cb1aa419ab77937e06428979d736fa
Muhammad Usama <mohammad.usama441@gmail.com>
2025-07-25 08:30:39 +0500

    Final Commit

--- app.config.js
+++ app.config.js
@@ -3,9 +3,15 @@ const appJson = require('./app.json')
 export default {
     expo:{
         ...appJson.expo,
+        android: {
+            package: "com.usama.moviemirror"
+        },
         extra: {
             TMDB_API_KEY: process.env.TMDB_API_KEY,
-            TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN
+            TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN,
+            eas: {
+                projectId: "1312a707-5278-48bb-bb73-1545b49c0abf"
+            }
         },
         plugins: ["expo-font"]
     }
